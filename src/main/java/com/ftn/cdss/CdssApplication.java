039d9b327bf27befc61396090725c5d6620a69fb
Sara Peric <>
2018-05-30 01:06:04 +0200

    Initial commit

---
+++ src/main/java/com/ftn/cdss/CdssApplication.java
@@ -0,0 +1,12 @@
+package com.ftn.cdss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdssApplication.class, args);
+	}
+}
