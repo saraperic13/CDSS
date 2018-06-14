23edd417d9c08569b2d6497754dc6fc9c243ac53
Sara Peric <>
2018-06-14 21:22:17 +0200

    Drools setup.

--- src/main/java/com/ftn/cdss/CdssApplication.java
+++ src/main/java/com/ftn/cdss/CdssApplication.java
@@ -1,12 +1,26 @@
 package com.ftn.cdss;
 
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CdssApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CdssApplication.class, args);
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("com.ftn.cdss","cdss.rules", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
 	}
 }
